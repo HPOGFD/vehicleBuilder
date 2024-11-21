36f14dc4687363f51259ef5cb627576d87904aa5
Harry Oyarvide <poyarvide87@yahoo.com>
2024-11-21 12:34:15 -0500

    syntax errors corrected

--- src/classes/Motorbike.ts
+++ src/classes/Motorbike.ts
@@ -1,13 +1,10 @@
 // Importing Vehicle and Wheel classes
-import { cp } from 'fs';
 import Vehicle from './Vehicle.js';
 import Wheel from './Wheel.js';
 
-// TODO: The Motorbike class should extend the Vehicle class
-class Motorbike extends Vehicle{
-  // TODO: Declare properties of the Motorbike class
-  // TODO: The properties should include vin, color, make, model, year, weight, top speed, and wheels
-  // TODO: The types should be as follows: vin (string), color (string), make (string), model (string), year (number), weight (number), topSpeed (number), wheels (Wheel[])
+// The Motorbike class should extend the Vehicle class
+class Motorbike extends Vehicle {
+  // Declare properties of the Motorbike class
   vin: string;
   color: string;
   make: string;
@@ -17,8 +14,7 @@ class Motorbike extends Vehicle{
   topSpeed: number;
   wheels: Wheel[];
 
-  // TODO: Create a constructor that accepts the properties of the Motorbike class
-
+  // Constructor accepting the properties of the Motorbike class
   constructor(
     vin: string,
     color: string,
@@ -29,10 +25,10 @@ class Motorbike extends Vehicle{
     topSpeed: number,
     wheels: Wheel[]
   ) {
-     // TODO: The constructor should call the constructor of the parent class, Vehicle
-    super();
+    // Call the constructor of the parent class, Vehicle
+    super(vin, color, make, model, year, weight, topSpeed, wheels);
 
-    // TODO: The constructor should initialize the properties of the Motorbike class
+    // Initialize the properties of the Motorbike class
     this.vin = vin;
     this.color = color;
     this.make = make;
@@ -40,44 +36,36 @@ class Motorbike extends Vehicle{
     this.year = year;
     this.weight = weight;
     this.topSpeed = topSpeed;
-    // TODO: The constructor should check if the wheels array has 2 elements and create 2 new default Wheel objects if it does not
 
-  // TODO: Implement the wheelie method
-      // TODO: The method should log the message "Motorbike [make] [model] is doing a wheelie!"
-
+    // Ensure the wheels array has 2 elements; create new default Wheel objects if it does not
     this.wheels = wheels.length === 2 ? wheels : [new Wheel(), new Wheel()];
-      console.log(`Motorbike ${this.make} ${this.model} is doing a wheelie!`);
   }
-  
-   
 
+  // Implement the wheelie method
+  wheelie(): void {
+    console.log(`Motorbike ${this.make} ${this.model} is doing a wheelie!`);
+  }
 
-
-  // TODO: Override the printDetails method from the Vehicle class
+  // Override the printDetails method from the Vehicle class
   override printDetails(): void {
-  // TODO: The method should call the printDetails method of the parent class
-  super.printDetails();
-  // TODO: The method should log the details of the Motorbike
-  console.log(`VIN: ${this.vin}`);
-  console.log(`Color: ${this.color}`);
-  console.log(`Make: ${this.make}`);
-  console.log(`Model: ${this.model}`);
-  console.log(`Year: ${this.year}`);
-  console.log(`Weight: ${this.weight}`);
-  console.log(`Top sped: ${this.topSpeed}`);
-  // TODO: The details should include the VIN, make, model, year, weight, top speed, color, and wheels
-  console.log(
-    `Wheel 1: ${this.wheels[0].getDiameter} inch with a ${this.wheels[0].getTireBrand} tire`
-  );
-  console.log(
-    `Wheel 2: ${this.wheels[1].getDiameter} inch with a ${this.wheels[1].getTireBrand} tire`
-  );
-  console.log(
-    `Wheel 3: ${this.wheels[2].getDiameter} inch with a ${this.wheels[2].getTireBrand} tire`
-  );
-  console.log(
-    `Wheel 4: ${this.wheels[3].getDiameter} inch with a ${this.wheels[3].getTireBrand} tire`
-  )
+    // Call the printDetails method of the parent class
+    super.printDetails();
+
+    // Log the details of the Motorbike
+    console.log(`VIN: ${this.vin}`);
+    console.log(`Color: ${this.color}`);
+    console.log(`Make: ${this.make}`);
+    console.log(`Model: ${this.model}`);
+    console.log(`Year: ${this.year}`);
+    console.log(`Weight: ${this.weight}`);
+    console.log(`Top Speed: ${this.topSpeed}`);
+    console.log(
+      `Wheel 1: ${this.wheels[0].getDiameter()} inch with a ${this.wheels[0].getTireBrand()} tire`
+    );
+    console.log(
+      `Wheel 2: ${this.wheels[1].getDiameter()} inch with a ${this.wheels[1].getTireBrand()} tire`
+    );
+  }
 }
 
 // Export the Motorbike class as the default export
