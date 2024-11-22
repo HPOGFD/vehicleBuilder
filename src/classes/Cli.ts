fef7e4ebd1c1bf39268f06662f77ceba8ca7af36
Harry Oyarvide <poyarvide87@yahoo.com>
2024-11-22 06:49:15 -0500

    Tow method. Cheked if Truck is selected as vehicle to tow

--- src/classes/Cli.ts
+++ src/classes/Cli.ts
@@ -294,8 +294,11 @@ class Cli {
       ])
       .then((answers) => {
         // TODO: check if the selected vehicle is the truck
+        if (answers.vehicleToTow instanceof Truck) {
         // TODO: if it is, log that the truck cannot tow itself then perform actions on the truck to allow the user to select another action
-        // TODO: if it is not, tow the selected vehicle then perform actions on the truck to allow the user to select another action
+
+          
+        }
       });
   }
 
